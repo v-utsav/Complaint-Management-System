e6f95bd0451fb2dcd1e82f041ec80a93fddc51be
v-utsav <79682229+v-utsav@users.noreply.github.com>
2024-08-11 07:52:27 +0530

    Update UserRepository.java

--- src/main/java/com/utsav/cms/repository/UserRepository.java
+++ src/main/java/com/utsav/cms/repository/UserRepository.java
@@ -1,1 +1,23 @@
+package com.utsav.cms.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.utsav.cms.model_bean.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	@Query("from User where fname = :name or "
+			+ "lname = :name or "
+			+ "mname = :name or "
+			+ "concat(fname, ' ', mname) = :name or "
+			+ "concat(fname, ' ', lname) = :name or "
+			+ "concat(mname, ' ', lname) = :name or "
+			+ "concat(fname, ' ', mname, ' ', lname) = :name")
+	List<User> search(String name);
+	
+	@Query("from User where lname = :lname")
+	List<User> searchByLname(String lname);
+}
 
