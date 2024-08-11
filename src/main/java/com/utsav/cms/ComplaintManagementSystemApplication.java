d14e838f2f1c0d881207b75819a74ecc3ca167a7
v-utsav <79682229+v-utsav@users.noreply.github.com>
2024-08-11 07:56:26 +0530

    Add files via upload

---
+++ src/main/java/com/utsav/cms/ComplaintManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.utsav.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintManagementSystemApplication.class, args);
+		System.out.println("Application Running!");
+	}
+
+}
