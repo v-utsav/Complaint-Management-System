2260fc318411e4627562d9dca8b78fc220277713
v-utsav <79682229+v-utsav@users.noreply.github.com>
2024-08-11 07:54:32 +0530

    Update UserServiceImpl.java

--- src/main/java/com/utsav/cms/service/impl/UserServiceImpl.java
+++ src/main/java/com/utsav/cms/service/impl/UserServiceImpl.java
@@ -1,1 +1,81 @@
+package com.utsav.cms.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.utsav.cms.exception.ResourceNotFoundException;
+import com.utsav.cms.model_bean.User;
+import com.utsav.cms.repository.UserRepository;
+import com.utsav.cms.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	private UserRepository userRepository;
+	
+	public UserServiceImpl(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
+	
+	@Override
+	public User saveUser(User user) {
+		return userRepository.save(user);
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+
+	@Override
+	public User getUserById(long id) {
+		return userRepository.findById(id).orElseThrow(() -> 
+				new ResourceNotFoundException("User", "Id", id));
+	}
+
+	@Override
+	public User updateUser(User user, long id) {
+		User existingUser = userRepository.findById(id).orElseThrow(() -> 
+				new ResourceNotFoundException("User", "Id", id));
+		existingUser.setFname(user.getFname());
+		existingUser.setMname(user.getMname());
+		existingUser.setLname(user.getLname());
+		existingUser.setSsnNo(user.getSsnNo());
+		existingUser.setPhoneNo(user.getPhoneNo());
+		existingUser.setDob(user.getDob());
+		existingUser.setEmail(user.getEmail());
+		existingUser.setBirthPlace(user.getBirthPlace());
+		existingUser.setDrivingLicense(user.getDrivingLicense());
+		existingUser.setDlName(user.getDlName());
+		existingUser.setGender(user.getGender());
+		existingUser.setDod(user.getDod());
+		
+		userRepository.save(existingUser);
+		return existingUser;
+		
+	}
+
+	@Override
+	public String deleteUser(long id) {
+		userRepository.findById(id).orElseThrow(() -> 
+				new ResourceNotFoundException("User", "Id", id));
+		userRepository.deleteById(id);
+		return "User Deleted Successfully!";
+	}
+
+	@Override
+	public List<User> search(String name) {
+		List<User> users = userRepository.search(name);
+		return users;
+	}
+
+	@Override
+	public List<User> searchByLname(String lname) {
+		List<User> users = userRepository.searchByLname(lname);
+		return users;
+	}
+	
+}
 
